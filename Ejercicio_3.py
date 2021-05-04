af424e81e3f28a11d6e48691166e8286bd4e1ac2
VIVIANA ANDREA ANDREA <>
2021-05-04 04:48:19 +0000

    Ejercicio 1

---
+++ Ejercicio_3.py
@@ -0,0 +1,12 @@
+A= float(input("Inserte el dato A:"))
+B= float(input("Inserte el dato B:"))
+C= float(input("Inserte el dato C:"))
+D= float(input("Inserte el dato D:"))
+if (D==0):
+  total1=(A-C)**2
+  print(total1)
+elif
+total2=((A-B**3)/D)
+print(total2)
+else
+print("No existe formula ")
